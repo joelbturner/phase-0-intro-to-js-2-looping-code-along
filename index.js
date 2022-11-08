a5621d09766fc5bac7905e27f729c7fa62ed5cfe
Joel Turner <joelbturner.mb@gmail.com>
2022-11-08 12:17:46 -0500

    commit tree c796ba66ecf1873551793955ef4b7fb4913c1f8f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(arrayOfNames, event){
+    const shoppingCart = []
+    for (let i = 0; i < arrayOfNames.length; i++){
+        shoppingCart.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!` );
+        debugger;
+    }
+    return shoppingCart
+
+}
+
+const myArrayOfNames = ["Charles", "Sal", "Alice"]
+const myEvent = "Christmas"
+    writeCards(myArrayOfNames,myEvent);
+
+
+function countDown(){
+let i = 10;
+while (i > -1) {
+    console.log(i--);
+    }
+return i
+}
+
